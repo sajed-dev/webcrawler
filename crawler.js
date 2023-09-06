dcb017892f6f4e54e6ade14e5c048ef306e96aa8
Sajed Owesat <sajed.o@ubi.shopping>
2023-09-06 18:15:30 +0300

    more fixes

--- crawler.js
+++ crawler.js
@@ -1,21 +1,30 @@
 import cheerio from 'cheerio';
 import got from 'got';
 import fs from 'fs';
+import uri from 'node-uri';
+
+let loadedUrls = [];
 
 const url = process.argv[2];
 
 if(!url)
     throw 'Url is mandatory';
 
+console.log('Url is: ', url);
+
+const parsedUrl = uri.parseURI(url);
+const host = parsedUrl.host;
+
 const depth = process.argv[3] || 0;
 
-
+console.log('Depth is: ', depth);
 
 let results = [];
 
 const load = async (_url, _depth) => {
 
 
+console.log(`Loading page url=${_url}     depth=${_depth}`);
 
     const {body} = await got(_url).catch(err => {
         //console.log(err);
@@ -29,7 +38,11 @@ const load = async (_url, _depth) => {
     $('img').get().map(img => {
 
         let imageUrl = img.attribs.src;
-        results.push({depth: _depth, imageUrl, sourceUrl: _url});
+        let item = {depth: _depth, imageUrl, sourceUrl: _url};
+
+        console.log('adding new item to results: ', item);
+
+        results.push(item);
 
     });
 
@@ -39,11 +52,19 @@ const load = async (_url, _depth) => {
 
     const links = $('a').get();
     for (const a of links) {
+
         if(a.attribs.href == _url) continue;
 
         let link = a.attribs.href.endsWith('/')? a.attribs.href.slice(0, -1) : a.attribs.href;
 
+        if(loadedUrls.includes(link)) continue;
+
         if(link == _url) continue;
+
+        //check if this link is external
+        const parsedLink = uri.parseURI(link);
+        if(host != parsedLink.host) continue;
+
 
         if (link.indexOf('http') != -1) {
             await load(link, _depth);
@@ -51,6 +72,8 @@ const load = async (_url, _depth) => {
             await load(url + link, _depth);
         }
     }
+
+    loadedUrls.push(_url);
 
 };
 
